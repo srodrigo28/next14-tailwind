27ec42ee4e3456df7170655b8935123b2e4767e7
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-06-26 20:12:45 -0300

    interfaces-seeds

---
+++ src/app/data/constants/usuarios.ts
@@ -0,0 +1,28 @@
+import { Usuario } from "@/core/model/Usuario";
+
+const usuarios: Usuario[] = [
+    {
+        id: "1",
+        nome: "Andress Silva",
+        email: "andresilva@gmail.com",
+        senha: "123456"
+    },
+    {
+        id: "2",
+        nome: "Alane Siqueira",
+        email: "alasiqueira@gmail.com",
+        senha: "123456"
+    },{
+        id: "3",
+        nome: "Beto Silva",
+        email: "bsilva@gmail.com",
+        senha: "123456"
+    },{
+        id: "4",
+        nome: "Cristiano Abuquerque",
+        email: "cristiab@gmail.com",
+        senha: "123456"
+    }
+]
+
+export default usuarios;
