269380d9029b4fff0875a7ebba61c21ddaa59398
SEBASTIAO RODRIGO <rodrigoexer1@gmail.com>
2024-06-28 22:38:56 -0300

    updated

---
+++ src/app/data/constants/produtos.ts
@@ -0,0 +1,35 @@
+// import Aula from "@/core/model/Aula";
+
+const produtos = [
+    // const produtos: Aula[] = [
+    {
+        id: "1",
+        nome: "React",
+        curso: "React Fundamentals",
+        duracao: 4000,
+        status: 'publicado'
+    },
+    {
+        id: "2",
+        nome: "Next.js",
+        curso: "Next.js Essentials",
+        duracao: 6000,
+        status: 'desativada'
+    },
+    {
+        id: "3",
+        nome: "TypeScript",
+        curso: "TypeScript Basics",
+        duracao: 3000,
+        status: 'rascunho'
+    },
+    {
+        id: "4",
+        nome: "CSS Grid",
+        curso: "CSS Grid Essentials",
+        duracao: 2000,
+        status: 'publicado'
+    }
+];
+
+export default produtos;
